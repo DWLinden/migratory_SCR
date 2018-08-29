b20b6dd233cc1894331bbc2030a8c958a24bdbc0
Daniel Linden <Daniel.Linden@nmfs.local>
2018-08-29 16:40:54 -0400

    updates

---
+++ simulation.R
@@ -0,0 +1,5 @@
+#-------------------------------------------------------
+# This file sets up a simulated migratory population of
+# North Atlantic right whales that move up and down
+# the coast following a sinusoidal curve.
+#-------------------------------------------------------
